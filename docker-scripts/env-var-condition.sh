12455cd91de9107cc7758b4acaa8eaf6096f977e
Jake McNeil <33610633+jstmcneil@users.noreply.github.com>
2021-06-08 16:51:15 -0400

    Update env-var-condition.sh

--- docker-scripts/env-var-condition.sh
+++ docker-scripts/env-var-condition.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 for f in results/*; do
   if [ -s $f ]; then
-	  python /scripts/failed-check-teams.py $1
+  	if $3:
+	  	python /scripts/failed-check-teams.py $1
+	  fi
     exit 0
   fi
 done
-python /scripts/passed-check-teams.py $1
+
+if $3:
+	python /scripts/passed-check-teams.py $1
+fi
