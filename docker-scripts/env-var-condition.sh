1ca55f078e32d6bf1f1d9140cebb1b866e579fca
Jake McNeil <33610633+jstmcneil@users.noreply.github.com>
2021-06-21 13:03:32 -0400

    Update env-var-condition.sh

--- docker-scripts/env-var-condition.sh
+++ docker-scripts/env-var-condition.sh
@@ -3,9 +3,9 @@ for f in results/*; do
   echo "1: $1"
   echo "2: ${2:-}"
   if [ -s $f ]; then
-    python /scripts/failed-check-teams.py ${2:-"default"} ${1:-}
+    python /scripts/failed-check-teams.py ${2:-default} ${1:-}
     exit 1
   fi
 done
-python /scripts/passed-check-teams.py ${2:"default"} ${1:-}
+python /scripts/passed-check-teams.py ${2:-default} ${1:-}
 exit 0
