3834a3d60fb0784868203b9af094a502820f36a2
Jake McNeil <33610633+jstmcneil@users.noreply.github.com>
2021-06-21 14:50:24 -0400

    Update run-cfn-binary.sh

--- docker-scripts/run-cfn-binary.sh
+++ docker-scripts/run-cfn-binary.sh
@@ -18,6 +18,7 @@ do
   	cfn-guard check -r $outputted -t $f >> results/${base_name}.txt
   fi
 done
+cat results/
 echo -e "\n"
 # Runs next step w/ webhook attached.
 bash /scripts/env-var-condition.sh ${2:-} ${3:-}
