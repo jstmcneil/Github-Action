c6040edd068e518f455105e66e6098e0cea448cc
Jake McNeil <33610633+jstmcneil@users.noreply.github.com>
2021-06-28 13:25:40 -0400

    Update failed-check-teams.py

--- docker-scripts/failed-check-teams.py
+++ docker-scripts/failed-check-teams.py
@@ -26,7 +26,7 @@ def colorSchemeIntialize():
     OFF_COLOR   = "\033[33m"  #GRAY
   elif sys.argv[1] == 'light':
     MAIN_COLOR  = "\033[30m"  #BLACK
-    OFF_COLOR   = "\033[37m"  #GRAY
+    OFF_COLOR   = "\033[33m"  #GRAY
   elif sys.argv[1] == 'default':
     MAIN_COLOR  = "\033[0m"   #WHITE
     OFF_COLOR   = "\033[37;1m"  #GRAY
