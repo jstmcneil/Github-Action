bd3fcde69693c9a93f2edd6da7ddf46834b7a379
Jake McNeil <jake.a.mcneil@pwc.com>
2021-06-28 17:21:02 -0400

    Full path name

--- docker-scripts/failed-check-teams.py
+++ docker-scripts/failed-check-teams.py
@@ -119,7 +119,7 @@ def onlyCommandLine(filename, count: int, total: int, listOfBad: list, listOfGoo
         if (number == 0):
           header = ("{}Errors for {}[".format(MAIN_COLOR, OFF_COLOR) + line.rstrip() + "]{} CloudFormation Template:".format(MAIN_COLOR))
         elif (number != (len(data) - 1)):
-          program.write('%d.  %s\n' % (number + 1, line))
+          program.write('%d.  %s\n' % (number, line))
           line = re.sub("(.{200})", "\\1\n    ", line, 0, re.DOTALL)
           line = re.sub(r'\[(.*?)\]', r'{}'.format(OFF_COLOR) + '\g<0>' + '{}'.format(MAIN_COLOR), line.rstrip())
           output = output + ('{}{:<3s} {:>7s}\n\n'.format(MAIN_COLOR, str(number+1)+".", line))
