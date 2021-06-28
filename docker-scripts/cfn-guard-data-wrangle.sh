a664f61eea7ec7c98f3b4cc121e12d30341f3129
Jake McNeil <33610633+jstmcneil@users.noreply.github.com>
2021-06-28 12:53:45 -0400

    Update cfn-guard-data-wrangle.sh

--- docker-scripts/cfn-guard-data-wrangle.sh
+++ docker-scripts/cfn-guard-data-wrangle.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 out=$(cfn-guard validate -r $1 -d $2 --show-summary none  --type CFNTemplate -o single-line-summary | sed '/^Rule/d' | sed '/^--/d' | sed '/^Evaluation of rules/d')
 numb="$(echo $out | grep "Resource \[" | wc -l | sed 's/^[ \t]*//')"
-cfn-guard validate -r $1 -d $2 --show-summary none  --type CFNTemplate -o single-line-summary | sed '/^Rule/d' | sed '/^--/d' | sed '/^Evaluation of rules/d'
+#cfn-guard validate -r $1 -d $2 --show-summary none  --type CFNTemplate -o single-line-summary | sed '/^Rule/d' | sed '/^--/d' | sed '/^Evaluation of rules/d'
+echo -e $out
 echo -e "\nTotal Failures: "  | sed -e '$s%$%'"$numb"'%'
